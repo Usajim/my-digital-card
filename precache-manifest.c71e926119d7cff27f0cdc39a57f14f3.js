4659f767e1899c1c654fafe33cd73defdab6adb0
Mary Usaji <mary.usaji@gmail.com>
2022-07-24 00:42:00 +0300

    Updates

---
+++ precache-manifest.c71e926119d7cff27f0cdc39a57f14f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9712a9b46962cb0931",
+    "url": "/my-digital-card/static/js/main.be9712a9.chunk.js"
+  },
+  {
+    "revision": "bbec7dc8d58ec3f5adc9",
+    "url": "/my-digital-card/static/js/1.bbec7dc8.chunk.js"
+  },
+  {
+    "revision": "35d1339b58c3d803c0ba",
+    "url": "/my-digital-card/static/js/runtime~main.35d1339b.js"
+  },
+  {
+    "revision": "28d96c5760b8e6aa2ed05044acf9810b",
+    "url": "/my-digital-card/index.html"
+  }
+];
